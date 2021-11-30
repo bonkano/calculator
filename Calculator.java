d6661fc0a33e9461ef89d1a10654be6376002a29
Mohamed Maiga <moha12792@gmail.com>
2021-11-30 23:24:30 +0000

    Adding method for doubles numbers

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,11 @@ public class Calculator {
 		
 	}
 	
-	public int addition(int a, intb) {
-		
+	public int addition(int a, int b) {
+		return a + b;
+	}
+	
+	public double addition(double a, double b) {
+		return a + b;
 	}
 }
